f7a165db75b8618ebe9fe197d7ec71f224e12dd8
Dionnessantos <137581799+Dionnessantos@users.noreply.github.com>
2023-06-23 22:38:10 -0300

    Add files via upload

---
+++ questao 04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+double questao5(double a1,double an,double n){
+    double pa = (n*(a1+ an))/2;
+
+    return pa;
+}
+int main (){
+
+    double n,a1,an;
+    scanf("%lf %lf %lf",&a1,&n,&n);
+    printf("%lf\n",questao5(a1,an,n));
+
+    return 0;
+}
