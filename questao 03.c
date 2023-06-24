22c7eb58951ac617688d35bb6d6a1c191c993855
Dionnessantos <137581799+Dionnessantos@users.noreply.github.com>
2023-06-23 22:37:21 -0300

    Add files via upload

---
+++ questao 03.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void imprimir(int i,int n)
+{
+    for(i=1; i<=n; i++)
+    {
+        printf(" %d\n",i);
+    }
+}
+int main ()
+{
+
+    int n,i;
+
+    scanf("%d",&n);
+    for (i=0; i<=n; i++){
+
+    }
+
+    imprimir(i,n);
+
+
+    return 0;
+}
